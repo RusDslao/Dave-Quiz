b0765f3c08ec9956a270cb83283bf61d0e1396a9
RusDslao <dasilaorussel.gmail.com>
2025-09-14 16:23:12 +0800

    test

---
+++ Subjects/Collections.js
@@ -0,0 +1,37 @@
+// Define Science questions
+const GB = [
+  {
+    question: "There are two ways to group objects in programming:",
+    options: [
+      "by creating arrays and objects.",
+      "by creating collections of objects.",
+      "be done with individual variablesO2",
+      "Both A and B",
+    ],
+    answer: "Both A and B",
+  },
+  {
+    question:
+      "Which of the following is the correct way to declare an integer array in C#?",
+    options: [
+      "int[] numbers;",
+      "int numbers[];",
+      "[]int numbers;",
+      "int[5] numbers;",
+    ],
+    answer: "int[] numbers;",
+  },
+  {
+    question:
+      "In C#, arrays are objects and must be instantiated before use. Which of the following statements is correct?",
+    options: [
+      "int[] numbers; numbers = new int[5];",
+      "int[] numbers = new int[5];",
+      "int[] numbers; numbers[0] = 1;",
+      "int numbers[5];",
+    ],
+    answer: "int[] numbers = new int[5];",
+  },
+];
+
+export default GB;
