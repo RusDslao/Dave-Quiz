03ad4611c968d7dc22073cfce6d0189dc5f8579e
RusDslao <dasilaorussel.gmail.com>
2025-09-14 18:26:42 +0800

    test again

--- Subjects/IS.js
+++ Subjects/IS.js
@@ -11,5 +11,16 @@ const IS = [
     ],
     answer: "D) Both A and C are correct.",
   },
+  {
+    question: "Which statement best describes a do…while loop?",
+    options: [
+      "A) Executes only if the condition is true initially.",
+      "B) Executes its body at least once, then repeats while the condition is true.",
+      "C) Repeats statements a fixed number of times.",
+      "D) A conditional operator for two expressions.",
+    ],
+    answer:
+      "B) Executes its body at least once, then repeats while the condition is true.",
+  },
 ];
 export default IS;
