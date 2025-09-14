aa0642b833e6aba3fe5f5a86bcb25801f6618e74
RusDslao <dasilaorussel.gmail.com>
2025-09-14 18:39:16 +0800

    test

--- Subjects/IS.js
+++ Subjects/IS.js
@@ -10,6 +10,16 @@ const IS = [
     answer:
       "B) Executes its body at least once, then repeats while the condition is true.",
   },
+  {
+    question: "Correct syntax for a do…while loop:",
+    options: [
+      "A)\ndo {\n    x++;\n    Console.WriteLine(x);\n} while (x <= 10);",
+      "B) do (x++); while (x <= 10);",
+      "C) while (x <= 10) { x++; Console.WriteLine(x); }",
+      "D) do x++; Console.WriteLine(x); while (x <= 10);",
+    ],
+    answer: "A)\ndo {\n    x++;\n    Console.WriteLine(x);\n} while (x <= 10);",
+  },
 ];
 
 export default IS;
